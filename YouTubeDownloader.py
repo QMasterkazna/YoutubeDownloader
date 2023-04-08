051d56a22ad943c752e5288b94d240beaf3cd954
QMaster <94114974+QMasterkazna@users.noreply.github.com>
2023-04-08 19:22:14 +0500

    youtube downloader

--- YouTubeDownloader.py
+++ YouTubeDownloader.py
@@ -4,13 +4,34 @@ from tkinter import ttk
 
 from pytube import YouTube
 
+import yt_dlp
+
 import os
 
 import json
+from moviepy.editor import *
+
 
 def clear():
     errmg2.set("Очищено")
     entry.delete(0, END)
+
+
+"""
+yt_opts = {
+    'verbose': True,
+    'download_sections': [{
+        'section': {
+            'start_time': 2,
+            'end_time': 7
+        }
+    }]
+}
+
+ydl = yt_dlp.YoutubeDL(yt_opts)
+
+ydl.download(video)
+"""
 
 
 def Download():
@@ -19,20 +40,29 @@ def Download():
         Pathfile = json.load(file)
     if video:
         try:
-            yt = YouTube(video)
+            errmg2.set("Downloading...")
+            linkVideo = video
+            yt = YouTube(linkVideo)
             file = yt.streams.get_lowest_resolution()
             oputh = (f"{Pathfile}")
-            file.download(oputh)
-            print("успешно")
-            errmg.set("Entry link pls:>")
+            file.download(output_path=oputh)
             errmg2.set(f"Successfully {yt.title}")
-            clear()
-        except:
+        except Exception as e:
+            print(e)
             print(Pathfile)
-            errmg2.set("Не указан путь. "+"Или файл уже есть в этой папке")
+            errmg2.set("Не указан путь. " + "Или файл уже есть в этой папке")
     else:
         errmg2.set("Поле ввода ссылки пустое")
         print("Empty")
+
+
+def MP4ToMP3(mp4, mp3):
+    FILETOCONVERT = AudioFileClip(mp4)
+    FILETOCONVERT.write_audiofile(mp3)
+    FILETOCONVERT.close()
+    print('YES CONVERTED!')
+    print('Ready')
+
 
 def DownloadMP3():
     video = entry.get()
@@ -40,34 +70,39 @@ def DownloadMP3():
         Pathfile = json.load(file)
     if video:
         try:
-            yt = YouTube(video)
-            file = yt.streams.filter(only_audio=True).first()
+            errmg2.set("Downloading...")
+            linkAudio = video
+            yt = YouTube(linkAudio)
+            file = yt.streams.get_lowest_resolution()
             oputh = (f"{Pathfile}")
             out_file = file.download(output_path=oputh)
-            # save the file
-            base, ext = os.path.splitext(out_file)
-            new_file = base + '.mp3'
-            os.rename(out_file, new_file)
+            convertToMp3 = oputh + f'/{yt.title}.mp3'
+            print(out_file)
+            print(oputh + f'/{yt.title}.mp3')  # делаем путь с mp3
+            MP4ToMP3(out_file, convertToMp3)  # convert to mp3
+            os.remove(out_file)
             errmg2.set(f"Successfully {yt.title}")
-        except:
+        except Exception as e:
+            print(e)
             print(Pathfile)
-            errmg2.set("Не указан путь. "+"Или файл уже есть в этой папке")
+            errmg2.set("Не указан путь. " + "Или файл уже есть в этой папке")
     else:
         errmg2.set("Поле ввода ссылки пустое")
         print("Empty")
 
+
 def choose():
     global directory
     directory = fd.askdirectory(title="Открыть папку", initialdir="/")
-    with open("path.json", 'w',encoding='utf-8' ) as file:
+    with open("path.json", 'w', encoding='utf-8') as file:
         json.dump(directory, file, ensure_ascii=False)
     with open('path.json', 'r', encoding='utf-8') as rfile:
         Pathfile = json.load(rfile)
     directory_choose.set(Pathfile)
 
-with open('path.json','r', encoding='utf-8') as PathtoReadFile:
-    pathToSave = json.load(PathtoReadFile)
 
+with open('path.json', 'r', encoding='utf-8') as PathtoReadFile:
+    pathToSave = json.load(PathtoReadFile)
 
 root = Tk()
 root.title("Youtube Downloader :>")
@@ -77,7 +112,7 @@ root.resizable(width=False, height=False)
 
 bg = PhotoImage(file="897-989.png")
 my_label = Label(root, image=bg)
-my_label.place(x=0,y=0, relwidth=1, relheight=1)
+my_label.place(x=0, y=0, relwidth=1, relheight=1)
 
 errmg2 = StringVar()
 errmg2.set("First job")
@@ -94,7 +129,7 @@ entry.pack(anchor=NW, padx=6, pady=6)
 btn = ttk.Button(text="Download", command=Download)
 btn.pack(anchor=NW, padx=6, pady=6)
 
-btn_mp3= ttk.Button(text="Download MP3", command=DownloadMP3)
+btn_mp3 = ttk.Button(text="Download MP3", command=DownloadMP3)
 btn_mp3.pack(anchor=NW, padx=6, pady=6)
 
 directory_choose = StringVar()
--- YouTubeDownloader.spec
+++ YouTubeDownloader.spec
@@ -41,5 +41,5 @@ exe = EXE(
     target_arch=None,
     codesign_identity=None,
     entitlements_file=None,
-    icon=['Youtube_Image.ico'],
+    icon=['YouTube_image.ico'],
 )
